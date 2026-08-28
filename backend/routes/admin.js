c2816131af90a93041d31b3c3a427281921cf3cf
tridib371 <stridib772@gmail.com>
2026-08-28 18:13:25 +0600

    Import mongoose in backend/routes/admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const { protect, requireAdmin } = require('../middleware/auth');
 const User = require('../models/User');
